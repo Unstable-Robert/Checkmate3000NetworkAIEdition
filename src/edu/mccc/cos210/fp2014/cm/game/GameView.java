c4d87c9c7a0dfd680eb58b21cd8a2c75d096c995
Collin Scangarella <collinscangarella@gmail.com>
2014-04-07 15:10:00 -0400

    Added mouselistener to gameview

--- src/edu/mccc/cos210/fp2014/cm/game/GameView.java
+++ src/edu/mccc/cos210/fp2014/cm/game/GameView.java
@@ -2,6 +2,8 @@ package edu.mccc.cos210.fp2014.cm.game;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -9,7 +11,7 @@ import javax.swing.JPanel;
 
 import edu.mccc.cos210.fp2014.cm.player.Player;
 
-public class GameView extends JPanel implements Observer, ActionListener{
+public class GameView extends JPanel implements Observer, ActionListener, MouseListener{
 
 	/**
 	 * 
@@ -31,6 +33,36 @@ public class GameView extends JPanel implements Observer, ActionListener{
 	}
 	@Override
 	public void repaint(){
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 }
