3bea69b07358a84fdb13185bce7acfe84787b3aa
collinscangarella <collinscangarella@users.noreply.github.com>
2014-04-09 11:46:30 -0400

    Update GamePart.java

--- src/edu/mccc/cos210/fp2014/cm/util/GamePart.java
+++ src/edu/mccc/cos210/fp2014/cm/util/GamePart.java
@@ -1,7 +1,7 @@
 package edu.mccc.cos210.fp2014.cm.util;
 
 /**
- * 
+ * An enumeration representing which portion of the game we are currently in, the beginning, middle or end.
  */
 public enum GamePart {
 	BEGINNING, MIDDLE, END
