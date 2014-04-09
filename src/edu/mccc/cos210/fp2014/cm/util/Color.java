b247f1043d5aed40e8e2d67a0dad5e7edfb4543d
collinscangarella <collinscangarella@users.noreply.github.com>
2014-04-09 11:45:40 -0400

    Update Color.java

--- src/edu/mccc/cos210/fp2014/cm/util/Color.java
+++ src/edu/mccc/cos210/fp2014/cm/util/Color.java
@@ -1,7 +1,7 @@
 package edu.mccc.cos210.fp2014.cm.util;
 
 /**
- * 
+ * An enumeration representing the two possible colors (sides) in the game, black and white.
  */
 public enum Color {
 	BLACK, WHITE
