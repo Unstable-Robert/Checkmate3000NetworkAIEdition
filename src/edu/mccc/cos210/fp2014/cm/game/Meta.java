637a4df9fe8da40b7448f2ab7fc09e077858284f
collinscangarella <collinscangarella@users.noreply.github.com>
2014-04-09 11:52:41 -0400

    Update Meta.java

--- src/edu/mccc/cos210/fp2014/cm/game/Meta.java
+++ src/edu/mccc/cos210/fp2014/cm/game/Meta.java
@@ -1,16 +1,26 @@
 package edu.mccc.cos210.fp2014.cm.game;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 import edu.mccc.cos210.fp2014.cm.util.GameType;
 
 /**
  * A list of information relevant to each board, including gametype, time left over,
  * scores (for the AI).
  */
+ @XmlRootElement
 public class Meta {
+	@XmlElement
 	private GameType gameType;
+	@XmlElement
 	private int p1Time;
+	@XmlElement
 	private int p2Time;
+	@XmlElement
 	private double whiteScore;
+	@XmlElement
 	private double blackScore;
+	@XmlElement
 	private int numPossibleMoves;
 }
