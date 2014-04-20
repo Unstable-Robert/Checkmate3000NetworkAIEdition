9c68bf07d2ac30301b5d306668ac7f06685d9af6
RedSqrl <ribanskys@gmail.com>
2014-04-20 16:57:01 -0400

    small doc change to my test
    small doc change to my test

--- src/edu/mccc/cos210/fp2014/cm/menu/TestMainMenuView.java
+++ src/edu/mccc/cos210/fp2014/cm/menu/TestMainMenuView.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * Test if the The main menu.
+ * Test the The main menu.
  * This menu allows a user to chose what type of game they want to play.
  * Types of game:
  *    Local game, Host game, Join game
