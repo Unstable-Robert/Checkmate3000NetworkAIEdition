892bfa863b7aa18c28097f9ec60049cd3365d373
Collin Scangarella <collinscangarella@gmail.com>
2014-04-22 02:28:23 -0400

    seriously, though. I've made three commits which have actually reverted to the original.

--- src/edu/mccc/cos210/fp2014/cm/piece/Piece.java
+++ src/edu/mccc/cos210/fp2014/cm/piece/Piece.java
@@ -77,7 +77,7 @@ public abstract class Piece implements Cloneable {
 		for (Piece p : bClone.getPieces()){
 			if(p.getColor() != pt.getOriginalPiece().getColor()){
 				for(PossibleTile move : p.getLazyTiles(bClone)){
-					if (move.getRemovePiece().equals(k)){
+					if (move.getX() == k.getX() && move.getY() == k.getY()){
 						return true;
 					}
 				}
