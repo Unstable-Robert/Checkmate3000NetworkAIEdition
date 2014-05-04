2e8d2a63ef69f55da6959f6657c0a8a9223ce818
Collin Scangarella <collinscangarella@gmail.com>
2014-05-03 20:20:52 -0400

    added simple eval

---
+++ src/edu/mccc/cos210/fp2014/cm/player/intelligence/SimpleEval.java
@@ -0,0 +1,17 @@
+package edu.mccc.cos210.fp2014.cm.player.intelligence;
+
+public class SimpleEval extends EvaluationAlgorithm {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void evaluate() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
